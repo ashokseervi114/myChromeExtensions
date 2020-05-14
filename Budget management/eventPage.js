0df05f280981bcf7ad5f4b52ad736c76487a54d8
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2020-05-14 16:58:27 +0530

    contextMenu updated

---
+++ Budget management/eventPage.js
@@ -0,0 +1,47 @@
+var contextMenuItem = {
+    
+    "id": "spendMoney",
+    "title": "SpendMoney",    //onRightClick Appearnace
+    "contexts": ["selection"]   //when to appear(on selection)
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value) {
+    return !isNaN(value) && 
+            parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value,10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    
+    if(clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+        if(isInt(clickData.selectionText)) {
+            chrome.storage.sync.get(['total','limit'],function(budget){
+                var newTotal =0;
+                if(budget.total) {
+                    
+                    newTotal +=parseInt(budget.total);
+                }
+                newTotal +=parseInt(clickData.selectionText);
+                chrome.storage.sync.set({'total':newTotal},function() {
+                    if(newTotal>= budget.limit) {
+                        
+                        var notifOptions = {
+                       type :"basic",
+                       iconUrl: "icon16.png",
+                       title:"Limit Reached",
+                       message:"Ahho, u have reached"
+                   };
+                   chrome.notifications.create('limitNotif',notifOptions);
+                        
+                        
+                    }
+                });
+                
+                
+            });
+        }
+    }
+    
+});
