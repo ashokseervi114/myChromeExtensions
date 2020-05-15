af91c3a509d8b9590f382745990a2bb2290088ae
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2020-05-15 15:38:07 +0530

    Update eventPage.js

--- Budget management/eventPage.js
+++ Budget management/eventPage.js
@@ -51,3 +51,6 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
 chrome.storage.onChanged.addListener(function(changes, storageName){
     chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString()});
 });
+
+
+
