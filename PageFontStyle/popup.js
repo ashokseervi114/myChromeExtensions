342072c76d5538b946426b3b2ec3495059022d39
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2020-05-15 20:03:18 +0530

    unstable content.js

---
+++ PageFontStyle/popup.js
@@ -0,0 +1,25 @@
+//$(function () {
+//    var color = $('#fontColor').val();
+//    $('#fontColor').on("change paste keyup",function () {
+//        color = $(this).val();
+//    });
+//    $('#btnChange').click(function() {
+//        chrome.tabs.query({active : true, currentWindow : true}, function(tabs) {
+//            chrome.tabs.sendMessage(tabs[0].id,{todo:  "changeColor",clickedColor : color})
+//        });
+//    });
+//});
+
+
+$(function(){
+    color = $('#fontColor').val();
+    $("#fontColor").on("change paste keyup", function() {
+        color = $(this).val(); 
+    });
+    
+   $('#btnChange').click(function(){      
+         chrome.tabs.query({active:true,currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {todo: "changeColor", clickedColor: color });
+        });
+   });
+});
