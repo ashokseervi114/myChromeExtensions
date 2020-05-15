2763d09bfb60b395536b5918910f78e54d427966
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2020-05-15 19:03:50 +0530

    Added ContentMenus and Restrications of eventJs

--- PageFontStyle/eventPage.js
+++ PageFontStyle/eventPage.js
@@ -1,4 +1,13 @@
-chrome.tabs.query({ active : true, currentWindow : true}, function (tabs) {
-    chrome.pageAction.show(tabs[0].id);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.todo == "showPageAction") {
+        chrome.tabs.query({ active : true, currentWindow : true}, function (tabs) {
+        chrome.pageAction.show(tabs[0].id);
+    
+        });
+        
+    }
+    
     
 });
+
+
